c2b45fec6b02b47dc002fc4f01a2e4b8e7980d76
Olga Belitskaya <safuolga@gmail.com>
2020-12-24 10:28:50 +0300

    update sklearn-clusters.ipynb

--- sklearn_clusters.py
+++ sklearn_clusters.py
@@ -10,26 +10,21 @@ Original file is located at
 """
 
 # Commented out IPython magic to ensure Python compatibility.
-from IPython.display import display,HTML
-def dhtml(str):
-    display(HTML("""<style>
-    @import 'https://fonts.googleapis.com/css?family=Akronim&effect=3d';      
-    </style><h1 class='font-effect-3d' 
-    style='font-family:Akronim; color:#aa33ff; font-size:35px;'>
-#     %s</h1>"""%str))
+# %run ../input/python-recipes/dhtml.py
+dhtml('Code Modules & Settings','#aa33ff',f2,fs8)
 
-dhtml('Code Modules & Settings')
-
-import warnings; warnings.filterwarnings('ignore')
+import warnings,time
+warnings.filterwarnings('ignore')
 import sklearn.datasets as sds
 import sklearn.cluster as scl
 import sklearn.metrics as sme
+import sklearn.manifold as sma
 import numpy as np,pandas as pd,pylab as pl
 from IPython.core.magic import register_line_magic
 from scipy.spatial.distance import pdist,squareform
 from scipy.cluster.hierarchy import linkage,dendrogram
 
-dhtml('Data')
+dhtml('Data','#aa33ff',f2,fs8)
 
 N=1000; n_clusters=7
 X,y=sds.make_blobs(n_samples=N,n_features=2, 
@@ -53,7 +48,22 @@ pl.scatter(X3[:,0],X3[:,1],
            edgecolor='#aa33ff',s=30)
 pl.tight_layout(); pl.grid(); pl.show()
 
-dhtml('Clustering')
+digits=sds.load_digits()
+X4,y4=digits.data,digits.target
+X4.shape,y4.shape
+
+n=10; img=np.zeros((10*n,10*n))
+for i in range(n): 
+    for j in range(n): 
+        img[(10*i+1):(10*i+9),
+            (10*j+1):(10*j+9)]=X4[i*n+j]\
+        .reshape((8,8))
+pl.figure(figsize=(5,5))
+pl.imshow(img,cmap=pl.cm.cool)
+pl.title('8x8 pixel images of digits')
+pl.xticks([]); pl.yticks([]);
+
+dhtml('Clustering','#aa33ff',f2,fs8)
 
 km,y_km=[],[]
 @register_line_magic
@@ -126,7 +136,7 @@ def silhouette_plots(n):
 # %kmeans_clusters 4
 # %silhouette_plots 4
 
-dhtml('Clusters as a Hierarchical Tree')
+dhtml('Clusters as a Hierarchical Tree','#aa33ff',f2,fs8)
 
 ac=scl.AgglomerativeClustering(n_clusters=3, 
                                affinity='euclidean', 
@@ -175,7 +185,7 @@ def dist_clusters(method):
 # Commented out IPython magic to ensure Python compatibility.
 # %dist_clusters True
 
-dhtml('DBSCAN')
+dhtml('DBSCAN, OPTICS, & Others','#aa33ff',f2,fs8)
 
 n_clusters=2
 km=scl.KMeans(n_clusters=n_clusters,random_state=0)
@@ -184,17 +194,23 @@ ac=scl.AgglomerativeClustering(n_clusters=n_clusters,
                                affinity='euclidean',
                                linkage='complete')
 y3_ac=ac.fit_predict(X3)
-db=scl.DBSCAN(eps=.2,min_samples=10,
+sp=scl.SpectralClustering(n_clusters=n_clusters)
+y3_sp=sp.fit_predict(X3)
+db=scl.DBSCAN(eps=.2,min_samples=15,
               metric='euclidean')
 y3_db=db.fit_predict(X3)
-cl=['KMeans','Agglomerative','DBSCAN']
-py3=[y3_km,y3_ac,y3_db]
+op=scl.OPTICS(eps=.2,min_samples=30)
+y3_op=op.fit_predict(X3)
+cl=['KMeans','Agglomerative','Spectral',
+    'DBSCAN','OPTICS']
+py3=[y3_km,y3_ac,y3_sp,y3_db,y3_op]
 
-f,ax=pl.subplots(3,1,figsize=(6,12))
-for c in range(3):
-    for i in range(n_clusters):
-        color=pl.cm.cool(float(i)/n_clusters)
-        ax[c].scatter(X3[py3[c]==i,0],X3[py3[c]==i,1],
+f,ax=pl.subplots(5,1,figsize=(6,18))
+for c in range(5):
+    for i in np.unique(py3[c]):
+        color=pl.cm.cool(.4*(i+1))
+        ax[c].scatter(X3[py3[c]==i,0],
+                      X3[py3[c]==i,1],
                s=30,color=color,marker='v',
                edgecolor='#aa33ff',
                label='cluster %d'%i)
@@ -202,4 +218,54 @@ for c in range(3):
     ax[c].set_xticks([]); ax[c].set_yticks([])
     ax[c].set_title(cl[c]);
 
-dhtml('In Progress')
+dhtml('TSNE & Clustering','#aa33ff',f2,fs8)
+
+t0=time.time()
+X4_emb=sma.TSNE(n_components=2,learning_rate=700.)\
+.fit_transform(X4)
+x_min,x_max=np.min(X4_emb,0),np.max(X4_emb,0)
+X4_emb=(X4_emb-x_min)/(x_max-x_min)
+f,ax=pl.subplots(1,figsize=(7,7))
+pl.axis("off")
+for i in range(X4_emb.shape[0]):
+    pl.text(X4_emb[i,0],X4_emb[i,1],str(y4[i]),
+            color=pl.cm.hsv(y4[i]/10.))
+pl.title("t-SNE embedding %f s"%(time.time()-t0))
+pl.tight_layout();
+
+n_clusters=10
+km=scl.KMeans(n_clusters=n_clusters,
+              init='k-means++',n_init=10,
+              max_iter=500,tol=1e-05,
+              random_state=0)
+y4_km=km.fit_predict(X4_emb)
+labs=[int(np.median(y4[np.where([y4_km==i])[1]]))
+      for i in range(10)]
+rd=dict(zip(list(range(10)),labs))
+cl=np.array([rd.get(x,x) for x in y4_km],
+                dtype='int8')
+
+f,ax=pl.subplots(1,figsize=(7,7))
+for i in range(n_clusters):
+    color=pl.cm.hsv(float(i)/n_clusters)
+    pl.scatter(X4_emb[cl==i,0],
+               X4_emb[cl==i,1],
+               s=30,color=color,
+               marker='$%d$'%i,
+               label='cluster %d'%i)
+pl.scatter(km.cluster_centers_[:,0],
+           km.cluster_centers_[:,1],
+           s=100, marker='*',c='black',
+           edgecolor='#aa33ff',label='centroids')
+pl.legend(scatterpoints=1); pl.tight_layout();
+
+dhtml('Interactive Experiments','#aa33ff',f2,fs8)
+
+# Commented out IPython magic to ensure Python compatibility.
+# %%html
+# <div id='smcell' style="border:10px double white; 
+#      width:680px; height:850px; overflow:auto; 
+#      padding:10px; background-color:ghostwhite">
+# <iframe id='if1' 
+# src="https://olgabelitskaya.gitlab.io/kaggle/kaggle_smc04.html" 
+# width="630" height="800"/></div>
